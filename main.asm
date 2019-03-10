fe043822f15c27d45a046a3512ff32df4c876982
Ammar Faizi <ammarfaizi2@gmail.com>
2019-03-10 22:59:28 +0700

    Initial commit

---
+++ main.asm
@@ -0,0 +1,112 @@
+
+section .data
+	aa0s db "Welcome to Tea Registration!",10
+	aa0l equ $-aa0s
+	aa1s db "Enter your name: "
+	aa1l equ $-aa1s
+	aa2s db "Berhasil!",10
+	aa2l equ $-aa2s
+	filename db "absensi.txt",0
+	in01l db 0,0,0
+
+section .bss
+	in01s resb 72
+	unixtime resb 12
+	file_dec resb 4	
+
+section .text
+	global _start
+
+p:
+	mov rax,1
+	mov rdi,1
+	syscall
+	ret
+
+_start:
+	call welcome
+	call get_name
+	call get_time
+	call write_to_file
+	call success
+	call exit
+
+welcome:
+	mov rsi,aa0s
+	mov rdx,aa0l
+	call p
+	ret
+
+get_name:
+	mov rsi,aa1s
+	mov rdx,aa1l
+	call p
+	mov rax,0
+	mov rdi,0
+	mov rsi,in01s
+	mov rdx,72
+	syscall
+	mov rdi,0
+	mov [in01s+rax+1],rdi
+	mov [in01l],eax
+	ret
+
+write_to_file:
+	mov rax,2
+	mov rdi,filename
+	mov rsi,1089
+	mov rdx,493
+	syscall
+	mov [file_dec],rax
+	mov rax,1
+	mov rdi,[file_dec]
+	mov rsi,unixtime
+	mov rdx,11
+	syscall
+	mov rax,1
+	mov rdi,[file_dec]
+	mov rsi,in01s
+	mov ax,[in01l]
+	mov rdx,rax
+	mov rax,1
+	syscall
+	ret
+
+get_time:
+	mov rax,201 ; time()
+	xor rdi,rdi
+	syscall
+	mov r8,0
+  .collect_num:
+  	inc r8
+  	mov rdx,0
+  	mov rbx,10
+  	div rbx
+  	push dx
+  	or eax,eax
+  	jne .collect_num
+  	dec r8
+  	mov r9,-1
+  	mov rdi,unixtime
+  .save:
+  	inc r9
+  	pop ax
+  	or al,0b110000
+  	mov [rdi],al
+  	add rdi,1
+  	cmp r9,r8
+  	jl .save
+  	mov byte [rdi], '|'
+	ret
+
+success:
+	mov rsi,aa2s
+	mov rdx,aa2l
+	call p
+	ret
+
+exit:
+	mov rax,60
+	xor rdi,rdi
+	syscall
+	ret
